960c46962ddeee90e8216a67d41c38569310c574
Anna-Gorina <gorinaanna1992@gmail.com>
2021-12-27 22:51:14 +0400

    add files

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 8795;
+        int costOfOneMile = 20;
+        int amountBonusMiles = ticketCost / costOfOneMile;
+        System.out.println(amountBonusMiles);
+
+
+    }
+}
+
+
